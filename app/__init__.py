6cec8682036f6b529e43cd4713fbbf13358ad9e2
abdullah <abdullahkhan14625@gmail.com>
2025-12-23 13:12:19 +0500

    Migrations for new Models, Celery Integration

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@ import os
 from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from app.constants import (DEFAULT_DATABASE_URI,DEFAULT_JWT_EXPIRATION_MINUTES,DEFAULT_SECRET_KEY)
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app(): 
@@ -27,8 +29,13 @@ def create_app():
         expiration_minutes = DEFAULT_JWT_EXPIRATION_MINUTES
     app.config["JWT_EXPIRATION_MINUTES"] = expiration_minutes
     db.init_app(app)
+    migrate.init_app(app, db)
+
+    from celery_app import celery_init_app
+    celery_init_app(app)
 
     from app.routes.auth import auth_bp
+    from app.models import User, CarMake, CarModel, CarYear
 
     app.register_blueprint(auth_bp)
 
